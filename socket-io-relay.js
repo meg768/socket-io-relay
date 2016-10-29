7f0a07ea2659f85d23ad7ff9647df6067dc04157
meg768 <magnus@egelberg.se>
2016-10-29 11:41:51 +0200

    -

--- socket-io-relay.js
+++ socket-io-relay.js
@@ -5,9 +5,88 @@ var Path = require('path');
 var mkpath = require('yow').mkpath;
 var sprintf = require('yow').sprintf;
 var isArray = require('yow').isArray;
+var readJSON = require('yow').readJSON;
 var redirectLogs = require('yow').redirectLogs;
 var prefixLogs = require('yow').prefixLogs;
 var cmd = require('commander');
+
+
+
+var App = function() {
+	cmd.version('1.0.0');
+	cmd.option('-l --log', 'redirect logs to file');
+	cmd.option('-p --port <port>', 'specifies port to listen to (3000)', 3000);
+	cmd.parse(process.argv);
+
+	prefixLogs();
+
+	if (cmd.log) {
+		var date = new Date();
+		var path = sprintf('%s/logs', __dirname);
+		var name = sprintf('%04d-%02d-%02d-%02d-%02d-%02d.log', date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
+
+		mkpath(path);
+		redirectLogs(Path.join(path, name));
+	}
+
+	console.log('Listening to port %d...', cmd.port);
+
+	var io = require('socket.io').listen(cmd.port);
+
+	var configFile = sprintf('%s/%s.config', __dirname, Path.parse(__filename).name);
+	var config = readJSON(configFile);
+	console.log(config);
+
+	for (var namespace in config.namespaces) {
+		console.log(namespace);
+
+		var consumerNamespace = io.of('/' + namespace);
+		var providerNamespace = io.of('/' + namespace + '-provider');
+
+		providerNamespace.on('connection', function(socket) {
+			console.log('New provider socket connection ', socket.id);
+
+			var events = config.namespaces[namespace].messages;
+
+			events.forEach(function(event) {
+				console.log('Defining event \'%s\'.', event);
+				socket.on(event, function(args) {
+					console.log('Sending event', event, args);
+					consumerNamespace.emit(event, args);
+				});
+
+			});
+
+			socket.emit('hello');
+
+
+		});
+
+		consumerNamespace.on('connection', function(socket) {
+			console.log('New consumer socket connection ', socket.id);
+
+			var messages = config.namespaces[namespace].messages;
+
+			messages.forEach(function(message) {
+				console.log('Defining message \'%s\'.', message);
+				socket.on(message, function(args) {
+					console.log('Sending message', message, args);
+					providerNamespace.to(provider.id).emit(message, args);
+				});
+
+			});
+
+			socket.emit('hello');
+
+
+		});
+	}
+
+
+};
+
+
+/*
 
 var App = function() {
 	cmd.version('1.0.0');
@@ -98,4 +177,5 @@ var App = function() {
 
 };
 
+*/
 new App();
